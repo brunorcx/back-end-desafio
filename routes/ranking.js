a3b236a9281401d503101bb8f23adb55bca602df
Bruno RC <brunorcx@hotmail.com>
2022-04-07 23:15:34 -0400

    fixed fields being arrays

--- routes/ranking.js
+++ routes/ranking.js
@@ -16,15 +16,18 @@ router.post("/", function (req, res, next) {
       "mongodb://admin:admin@basecluster-shard-00-00.hirbx.mongodb.net:27017,basecluster-shard-00-01.hirbx.mongodb.net:27017,basecluster-shard-00-02.hirbx.mongodb.net:27017/rankingDB?ssl=true&replicaSet=atlas-12fzsk-shard-0&authSource=admin&retryWrites=true&w=majority"
     );
     const rankingSchema = new mongoose.Schema({
-      playerName: [String],
-      score: [Number],
-      date: [String],
+      playerName: String,
+      score: String,
+      date: String,
     });
     const Rank = mongoose.model("Rank", rankingSchema);
     const form = new multiparty.Form();
     form.parse(req, async (err, fields, files) => {
-      console.log(await fields);
-      const newRankDocument = new Rank(fields);
+      var stringFields = new Object();
+      for (var [key, value] of Object.entries(fields)) {
+        stringFields[key] = value.toString();
+      }
+      const newRankDocument = new Rank(stringFields);
       // save the document
       await newRankDocument.save();
       res.send("Got a POST Ranking request ");
@@ -37,6 +40,24 @@ router.post("/", function (req, res, next) {
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("Got a  GET Ranking request ");
+  //Moongoose
+  main().catch((err) => console.log(err));
+
+  async function main() {
+    await mongoose.connect(
+      "mongodb://admin:admin@basecluster-shard-00-00.hirbx.mongodb.net:27017,basecluster-shard-00-01.hirbx.mongodb.net:27017,basecluster-shard-00-02.hirbx.mongodb.net:27017/rankingDB?ssl=true&replicaSet=atlas-12fzsk-shard-0&authSource=admin&retryWrites=true&w=majority"
+    );
+    const rankingSchema = new mongoose.Schema({
+      playerName: [String],
+      score: [Number],
+      date: [String],
+    });
+    const Rank = mongoose.model("Rank", rankingSchema);
+    const form = new multiparty.Form();
+    //retrieve all documents
+    const ranksDocuments = await Rank.find();
+    res.send(ranksDocuments);
+  }
+  //End Moongoose
 });
 module.exports = router;
