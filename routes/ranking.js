5b0df884140328eaf9210bf41bf025c28d997f28
Bruno RC <brunorcx@hotmail.com>
2022-04-07 22:55:45 -0400

    saving document of player score in mongoDB Atlas

---
+++ routes/ranking.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+const multiparty = require("multiparty");
+const mongoose = require("mongoose");
+
+// router.post("/ranking", (req, res, next) => {
+//   res.send("Got a POST request");
+// });
+
+router.post("/", function (req, res, next) {
+  //Moongoose
+  main().catch((err) => console.log(err));
+
+  async function main() {
+    await mongoose.connect(
+      "mongodb://admin:admin@basecluster-shard-00-00.hirbx.mongodb.net:27017,basecluster-shard-00-01.hirbx.mongodb.net:27017,basecluster-shard-00-02.hirbx.mongodb.net:27017/rankingDB?ssl=true&replicaSet=atlas-12fzsk-shard-0&authSource=admin&retryWrites=true&w=majority"
+    );
+    const rankingSchema = new mongoose.Schema({
+      playerName: [String],
+      score: [Number],
+      date: [String],
+    });
+    const Rank = mongoose.model("Rank", rankingSchema);
+    const form = new multiparty.Form();
+    form.parse(req, async (err, fields, files) => {
+      console.log(await fields);
+      const newRankDocument = new Rank(fields);
+      // save the document
+      await newRankDocument.save();
+      res.send("Got a POST Ranking request ");
+    });
+    //retrieve all documents
+    // const ranksDocuments = await Rank.find();
+  }
+  //End Moongoose
+});
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("Got a  GET Ranking request ");
+});
+module.exports = router;
